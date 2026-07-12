ba304501caa50496666ad6f9b2fe9afe947a86cb
Shivanshu Singh <88299798+ShivanshuSingh19manit@users.noreply.github.com>
2026-07-12 16:10:23 +0530

    Add: single-element-in-a-sorted-array - submission-5

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while(l<r){
+            int mid = l + (r-l)/2;
+            if((mid & 1) == 1){
+                mid--;
+            }
+            if(nums[mid] != nums[mid + 1]){
+                r = mid;
+            } else {
+                l = mid + 2;
+            }
+        }
+        return nums[l];
+    }
+}
